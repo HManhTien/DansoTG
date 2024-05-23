e7901ac9709a8a34031159db1d78226bdd54d2b6
neit <neit@DESKTOP-F4L41KI>
2024-05-23 21:09:59 +0700

    Vietwweb

---
+++ DansoTG/WEB/WebDanso/WebDanso/Asset/MyCode.js
@@ -0,0 +1,79 @@
+﻿$(document).ready(function () {
+    const api = '/api.aspx';
+    $.post(api, { action: 'LIST_DAN_SO' }, function (data) {
+
+        var json = JSON.parse(data);
+        var rawData = json.data;
+       
+        // Tìm dân số lớn nhất của mỗi quốc gia
+        var maxPopulationByCountry = {};
+        var populationByCountryAndYear = {};
+
+        rawData.forEach(function (item) {
+            var country = item.QUOC_GIA;
+            var year = parseInt(item.NAM, 10) + 2;
+            var population = parseInt(item.DAN_SO);
+
+            if (!maxPopulationByCountry[country] || maxPopulationByCountry[country] < population) {
+                maxPopulationByCountry[country] = population;
+            }
+
+            if (!populationByCountryAndYear[country]) {
+                populationByCountryAndYear[country] = {};
+            }
+            populationByCountryAndYear[country][year] = population;
+        });
+
+        // Hiển thị bảng dân số lớn nhất của mỗi quốc gia
+        var tableHtml = '<table class="table table-bordered"><thead><tr><th>Country</th><th>Max Population</th></tr></thead><tbody>';
+        for (var country in maxPopulationByCountry) {
+            tableHtml += '<tr><td>' + country + '</td><td>' + maxPopulationByCountry[country] + '</td></tr>';
+        }
+        tableHtml += '</tbody></table>';
+        $('#max_population_table').html(tableHtml);
+
+        // Chuẩn bị dữ liệu cho biểu đồ
+        var years = [];
+        for (var year in populationByCountryAndYear["World"]) {
+            years.push(year);
+        }
+        years.sort();
+
+        var chartData = [['Year']];
+        for (var country in populationByCountryAndYear) {
+            chartData[0].push(country);
+        }
+
+        years.forEach(function (year) {
+            var row = [year];
+            for (var country in populationByCountryAndYear) {
+                row.push(populationByCountryAndYear[country][year] || 0);
+            }
+            chartData.push(row);
+        });
+
+        // Vẽ biểu đồ dân số qua các năm
+        google.charts.load('current', { 'packages': ['corechart'] });
+        google.charts.setOnLoadCallback(function () {
+            var data = google.visualization.arrayToDataTable(chartData);
+
+            var options = {
+                title: 'Population Over Years',
+                curveType: 'function',
+                legend: { position: 'bottom' },
+                hAxis: {
+                    title: 'Year',
+                    titleTextStyle: { color: '#333' }
+                },
+                vAxis: {
+                    title: 'Population',
+                    minValue: 0
+                }
+            };
+
+            var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+            chart.draw(data, options);
+        });
+    })
+    
+});
---
+++ DansoTG/WEB/WebDanso/WebDanso/api.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SuatAn;
+
+namespace WebDanso
+{
+    public partial class api : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string action = Request["action"];
+            switch (action)
+            {              
+                case "LIST_DAN_SO":
+                    xu_ly(action);
+                    break;
+            }
+        }
+        void xu_ly(string action)
+        {
+            SqlServer db = new SqlServer();
+            SqlCommand cm = db.GetCmd("SP_DANSO", action); //tạo cm với "SP_Company" và @action từ method GetCmd của db
+            switch (action)
+            {
+                case "LIST_DAN_SO":
+                    break;             
+            }
+            string json = (string)db.Scalar(cm); //thuc thi SqlCommand cm này để thu về json
+            Response.Write(json); //trả json về trình duyệt
+        }
+    }
+}
